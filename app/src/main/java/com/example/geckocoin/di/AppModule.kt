fef59ae0357da2170b609cfe852fa59f99b0b2a9
kanake10 <10kanake@gmail.com>
2022-06-20 21:42:34 +0300

    di

---
+++ app/src/main/java/com/example/geckocoin/di/AppModule.kt
@@ -0,0 +1,53 @@
+package com.example.geckocoin.di
+
+
+import com.example.geckocoin.domain.repository.ExchangesRepository
+import com.example.geckocoin.core.Constants.BASE_URL
+import com.example.geckocoin.data.api.CoinGeckoApi
+import com.example.geckocoin.data.repository.ExchangesRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Provides
+    fun providesHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Provides
+    fun providesOkhttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .callTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+        return okHttpClient.build()
+    }
+
+    @Provides
+    fun provideCoinGeckoApi(): CoinGeckoApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinGeckoApi::class.java)
+    }
+    @Provides
+    fun provideExchangesRepository(coinGeckoApi: CoinGeckoApi): ExchangesRepository {
+        return ExchangesRepositoryImpl(coinGeckoApi)
+    }
+
+
+}
