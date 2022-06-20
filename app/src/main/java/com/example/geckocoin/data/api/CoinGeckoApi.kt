d1d65be2be96f0f47b71a7c16a309e69580fc44e
kanake10 <10kanake@gmail.com>
2022-06-20 21:43:10 +0300

    api

---
+++ app/src/main/java/com/example/geckocoin/data/api/CoinGeckoApi.kt
@@ -0,0 +1,16 @@
+package com.example.geckocoin.data.api
+
+import com.example.geckocoin.data.dto.ExchangesDetailsDto
+import com.example.geckocoin.core.Constants.EXCHANGES_DETAILS_ENDPOINT
+import com.example.geckocoin.core.Constants.EXCHANGES_ENDPOINT
+import com.example.geckocoin.data.dto.ExchangesDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CoinGeckoApi {
+    @GET(EXCHANGES_ENDPOINT)
+    suspend fun getExchanges():List<ExchangesDto>
+
+    @GET(EXCHANGES_DETAILS_ENDPOINT)
+    suspend fun getExchangesDetails(@Path("id") exchangesId: String): ExchangesDetailsDto
+}
