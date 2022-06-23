9834cce95716eddf789e88760163da218b4c2d21
kanake10 <10kanake@gmail.com>
2022-06-24 00:22:51 +0300

    added todo

--- app/src/main/java/com/example/geckocoin/presentation/screens/details/ExchangesDetailsScreen.kt
+++ app/src/main/java/com/example/geckocoin/presentation/screens/details/ExchangesDetailsScreen.kt
@@ -22,40 +22,60 @@ import com.example.geckocoin.presentation.screens.exchanges.ExchangesViewModel
 import com.example.geckocoin.ui.theme.GeckoCoinTheme
 
 
+/**
+ * This composable has not been worked on as per the requirements
+ * this is the detail's page
+ */
+
+/**
+val twitter_handle: String,
+val url: String,
+val year_established: Int,
+val country: String?,
+val image: String,
+val name: String,
+val description: String?,
+ */
+
 @Composable
 fun ExchangesDetailsScreen(
     viewModel: ExchangesDetailsViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
-    val scrollState = rememberScrollState()
 
-    state.exchangesDetails?.let { exchangesDetails ->
-        Column(
-            Modifier
-                .verticalScroll(scrollState)
-                .fillMaxSize()
-                .padding(12.dp)
-        ) {
-
-            Text(text = exchangesDetails.name)
-            Image(
-                modifier = Modifier
-                    .width(190.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .height(190.dp),
-                painter = rememberImagePainter(
-                    data = exchangesDetails.image.replace(
-                        "small",
-                        "large"
-                    )
-                ),
-                contentDescription = null,
-                contentScale = ContentScale.Crop
-            )
+    Box (
+        Modifier.fillMaxSize()
+            ){
+        state.exchangesDetails?.let { exchangesDetails ->
+            LazyColumn(
+                Modifier
+                    .fillMaxSize()
+                    .padding(12.dp)
+            ) {
+                item {
+                    Column (
+                        Modifier.fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center
+                            ){
+                        Text(text = exchangesDetails.name)
+                        Image(
+                            modifier = Modifier
+                                .width(190.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                                .height(190.dp),
+                            painter = rememberImagePainter(
+                                data = exchangesDetails.image.replace(
+                                    "small",
+                                    "large"
+                                )
+                            ),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                }
+            }
         }
-
-
-
     }
 }
 
